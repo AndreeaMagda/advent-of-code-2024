b2bd57363bb3802eb2896b824d77048bad6cffd7
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-12-05 19:23:31 +0200

    Part2

--- day2/main.py
+++ day2/main.py
@@ -2,8 +2,7 @@ file_path = "input.txt"
 
 with open(file_path, "r") as file:
     data = file.read()
-
-reports = [list(map(int, line.split())) for line in data.strip().split("\n")]
+reports = [list(map(int, line.split())) for line in data.strip().split("\n") if line.strip()]
 
 def is_safe_report(report):
     differences = [report[i + 1] - report[i] for i in range(len(report) - 1)]
@@ -19,9 +18,22 @@ def is_safe_report(report):
     return increasing or decreasing
 
 
+def remove(report):
+    for i in range(len(report)):
+        modified_report = report[:i] + report[i + 1:]
+        if is_safe_report(modified_report):
+            return True
+    return False
+
+
+
 safe_reports_count = 0
 for report in reports:
-    if is_safe_report(report):
+    if is_safe_report(report) or remove(report):
         safe_reports_count += 1
 
 print(safe_reports_count)
+
+
+
+
